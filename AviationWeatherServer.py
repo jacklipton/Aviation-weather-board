9db9dfe223d92e70ae47ffc363dc3013a3f6917e
kcaj106 <kcaj106@gmail.com>
2024-04-11 16:33:03 -0400

    Use of API Flt Cat Feature

--- AviationWeatherServer.py
+++ AviationWeatherServer.py
@@ -26,60 +26,29 @@ def AvWeather():
   airports = "KOGS,KMSS,CYOW,CYND,CYGK,CYTR,CYPQ,CYOO,CYQA,CYTZ,CYYZ,CYSN,KIAG,KBUF,CYHM,CYKF,CYXU"
 
   ltOrder = [14,41,44,8,21,7,19,26,35,17,31,48,32,38,36,4,1]
-  #colAdd = np.empty(len(airports))
-  #status = np.column_stack([airports, colAdd])
-  #skyCond = ["SKC", "CLR","FEW", "SCT", "BKN", "OVC","VV"]
+
   i = 0
 
-  print("test")
   hdr = {"X-API-Key": "e99ef131b8f04bc9853ddaed7f"}
-  r = requests.request("GET","https://api.checkwx.com/metar/"+ airports, headers=hdr)
+  r = requests.request("GET","https://api.checkwx.com/metar/"+ airports +"/decoded", headers=hdr)
 
   cyData = json.loads(r.text)['data']
 
 
   for cy in cyData:
-    metarParts = cy.split(" ")
-    for part in metarParts:
 
-      if 'SM' in part:
-        if '/' in part:
-          vis = 1
-        elif int(part[:-2])>= 5:
-          vis = 4
-        elif int(part[:-2])>= 3 and int(part[:-2])< 5:
-          vis = 3
-        elif int(part[:-2])>= 1 and int(part[:-2])< 3:
-          vis = 2
-        else:
-          vis = 1
-      if "SKC" in part or "FEW" in part or "SCT" in part or "BKN" in part or "OVC" in part or "VV0" in part or "CLR" in part:
-        if "BKN" in part or "OVC" in part or "VV" in part:
-          if int(removeLet(part)) > 30:
-            cloud = 4
-          elif int(removeLet(part)) > 10 and int(removeLet(part))<=30:
-            cloud = 3
-          elif int(removeLet(part)) > 5 and int(removeLet(part))<=10:
-            cloud = 2
-          elif int(removeLet(part)) < 5:
-            cloud = 1
-          break
-        else:
-          cloud = 4
-
-    if cloud == 4 and vis == 4:
+    if cy['flight_category'] == "VFR":
       print("VFR")
       colours("VFR",ltOrder[i])
-    else:
-      if min(cloud,vis) == 3:
-        print("MVFR")
-        colours("MVFR",ltOrder[i])
-      elif min(cloud,vis) == 2:
-        print("IFR")
-        colours("IFR",ltOrder[i])
-      elif min(cloud,vis) == 1:
-        print("LIFR")
-        colours("LIFR",ltOrder[i])
+    elif cy['flight_category'] == "MVFR":
+      print("MVFR")
+      colours("MVFR",ltOrder[i])
+    elif cy['flight_category'] == "IFR":
+      print("IFR")
+      colours("IFR",ltOrder[i])
+    elif cy['flight_category'] == "LIFR":
+      print("LIFR")
+      colours("LIFR",ltOrder[i])
 
     print(cy)
     i=i+1
